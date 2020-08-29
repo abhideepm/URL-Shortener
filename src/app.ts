f25893c66ce483a4f7d851e2318adcbf7909114e
Abhideep Maity <abhideepm@gmail.com>
2020-08-29 21:58:04 +0530

    add node typescript

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello')
+})
+
+app.listen(PORT, () => console.log('Listening'))
