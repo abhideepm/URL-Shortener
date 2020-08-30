2d8ef62343442bf4befaf288e275130cb674e747
Abhideep Maity <abhideepm@gmail.com>
2020-08-30 16:25:54 +0530

    add login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from 'react-router-dom'
+import Login from './Components/Login'
+import Register from './Components/Register'
 
 const App: React.FC = () => {
-	return <div></div>
+	return (
+		<div>
+			<Router>
+				<Switch>
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+					<Redirect from="/" to="/login" />
+				</Switch>
+			</Router>
+		</div>
+	)
 }
 
 export default App
---
+++ client/src/Components/Login.tsx
@@ -0,0 +1,79 @@
+import {
+	Box,
+	Button,
+	Card,
+	CardContent,
+	CssBaseline,
+	Grid,
+	TextField,
+	Typography,
+} from '@material-ui/core'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Link, useHistory } from 'react-router-dom'
+
+const Login: React.FC = () => {
+	const history = useHistory()
+	const { register, handleSubmit } = useForm()
+
+	const onSubmit = (data: JSON): void => {
+		console.log(data)
+	}
+	return (
+		<>
+			<CssBaseline />
+			<Grid
+				container
+				spacing={0}
+				alignItems="center"
+				justify="center"
+				style={{ minHeight: '100vh' }}
+			>
+				<Grid item xs={12} sm={9} md={6} lg={4}>
+					<Card raised={true} style={{ borderRadius: '1rem' }}>
+						<CardContent>
+							<Typography variant="h4" align="center">
+								Login
+							</Typography>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<Box my={2}>
+									<TextField
+										name="email"
+										label="Email"
+										variant="outlined"
+										inputRef={register}
+										fullWidth={true}
+										required
+									/>
+								</Box>
+								<Box my={2}>
+									<TextField
+										name="password"
+										label="Password"
+										variant="outlined"
+										type="password"
+										inputRef={register}
+										fullWidth={true}
+										required
+									/>
+								</Box>
+								<Box display="flex" justifyContent="center" my={2}>
+									<Button variant="contained" color="primary" type="submit">
+										Sign In
+									</Button>
+								</Box>
+								<Link to="/register">
+									<Typography display="block" align="center">
+										Register Instead
+									</Typography>
+								</Link>
+							</form>
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
+		</>
+	)
+}
+
+export default Login
