b180bc459c8886a170acd953e65dfeb2c319d46a
Abhideep Maity <abhideepm@gmail.com>
2020-09-01 10:52:09 +0530

    add deployment settings

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, { Application } from 'express'
 import mongoose from 'mongoose'
 import indexRouter from './routes/index'
 import cors from 'cors'
+import path from 'path'
 dotenv.config()
 
 const dbURL = <string>process.env.db_url
@@ -20,5 +21,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.json(), cors())
 app.use('/api', indexRouter)
+
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')))
+
+	// Handle React routing, return all requests to React app
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+	})
+}
 
 app.listen(PORT, () => console.log('Listening'))
